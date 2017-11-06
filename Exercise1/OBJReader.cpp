7a0355741984f226a31114f4e3373b3d63925b17
Marko Nygard <oi12mnd@cs.umu.se>
2017-11-06 12:22:15 +0100

    running on school comp

---
+++ Exercise1/OBJReader.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+/* Put float values from a string in a vector */
+vector<float> getFloats(string line) {
+	istringstream iss(line);
+	vector<float> values;
+  string token;
+  while (iss >> token) {
+    values.push_back(stof(token));
+	}
+	return values;
+}
+
+/* Put int values from a string in a vector */
+vector<string> getStrings(string line) {
+	istringstream iss(line);
+	vector<string> strings;
+  string token;
+  while (iss >> token) {
+    strings.push_back(token);
+	}
+	return strings;
+}
+
+/*  */
+void formatData(string filename){
+
+	// Open file for reading
+	ifstream data(filename);
+
+	// Read a line at a time
+  vector <vector<float>> vertices, normals, textures;
+  vector <vector<string>> faces;
+
+  bool triangulationExists = false;
+	string line;
+	if (data.is_open()) {
+		while (getline(data, line)) {
+			char tag = line[0];
+			if (tag == 'v') {
+        if (line[1] == 'n'){
+          line = line.substr(2, line.size());
+          normals.push_back(getFloats(line));
+        }
+        else if(line[1] == 't'){
+          line = line.substr(2, line.size());
+          textures.push_back(getFloats(line));
+        }
+        else{
+          line = line.substr(1, line.size());
+          vertices.push_back(getFloats(line));
+        }
+      }
+      else if(tag == 'f'){
+        triangulationExists = true;
+        // Handle this case separately
+        line = line.substr(1, line.size());
+        faces.push_back(getStrings(line));
+      }
+		}
+		data.close();
+	}
+	else{
+    cout << "Unable to open file\n";
+    exit(1);
+  }
+
+  if(! triangulationExists){
+    // Create your own triangulation
+    cout << "Oh no!!\n";
+  }
+
+  // Normalizeing the object?
+
+  cout << vertices.size() << endl;
+  cout << normals.size() << endl;
+  cout << faces.size() << endl;
+  cout << textures.size() << endl;
+
+  for (std::vector<vector<float>>::iterator it1 = vertices.begin() ; it1 != vertices.end(); ++it1){
+    vector<float> row = *it1;
+    for (std::vector<float>::iterator it2 = row.begin() ; it2 != row.end(); ++it2){
+      std::cout << *it2 << ' ';
+    }
+    std::cout << '\n';
+  }
+}
+
+int main(){
+  string filename = "cube.obj.txt";
+	formatData(filename);
+}
+
+
+
+
+
+
+
+
+
+
+//
